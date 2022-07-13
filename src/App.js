618c4c87d7be671f0145b7b0d8614e3f0f8332b9
xmmcdani <xavion.mcdaniels@gmail.com>
2022-07-12 22:32:37 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
 
 import Header from './components/Header/Header';
@@ -8,9 +9,20 @@ import Map from './components/Map/Map';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+           <Grid container spacing={3} style={{ width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+            <Grid item xs={12} md={8}>
+                    <Map />
+            </Grid>
+            </Grid>
+
+
+        </>
     );
 
 }
